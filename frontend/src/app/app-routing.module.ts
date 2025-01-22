43f402b00cb2fc6196e818e8791d92d661e5b6f4
Jetnipat yongyuth <jetnipat.y@ku.th>
2025-01-22 20:24:25 +0700

    firststep

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/mainpage', pathMatch: 'full' },
+  { path: 'mainpage', component: MainpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
