7e483796276f6c04383d1f07b1d7f00a7bde8961
poppy <poppypopcorn@gmail.com>
2025-01-23 21:54:07 +0700

    add gitignore(p)

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8076e48dcde5e2b3df5d178a51b868e2b7f9bc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0370d9769582ac3d92244672ce583c7f1621669b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
