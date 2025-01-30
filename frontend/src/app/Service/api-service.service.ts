129e1683f0697a488eb6bd482b233d6a608b002c
poppy <poppypopcorn@gmail.com>
2025-01-30 20:18:34 +0700

    card

--- frontend/src/app/Service/api-service.service.ts
+++ frontend/src/app/Service/api-service.service.ts
@@ -14,4 +14,8 @@ export class ApiServiceService {
     console.log("hello register ser+++++++++++++++vice");
     return this.http.post(`${apiUrl}/api/register/register`,registerData);
   }
+  getMainDish():Observable<any>{
+    console.log("Hello get maindish service");
+    return this.http.get(`${apiUrl}/api/Menu/getMainDish`);
+  }
 }
